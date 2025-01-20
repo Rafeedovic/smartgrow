b8cec4ebc52d6bae512b309df5d9f9cabee10167
Rafed BEN JEMAA <rafeedbj1@gmail.com>
2025-01-20 15:45:00 +0100

    adding sensors to the webpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SensorDataService } from './services/sensor-data.service';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,15 @@ import { SensorDataService } from './services/sensor-data.service';
 export class AppComponent implements OnInit {
   sensorData: any = {}; // Initialisation vide
   title = 'smartgrow';  // Définir la propriété title
+  lampStatus: boolean = false;
+  ventiloStatus: boolean = false;
+  irrigationStatus: boolean = false;
+  porteStatus: boolean = false;
 
-  constructor(private sensorDataService: SensorDataService) {}
+  constructor(
+    private sensorDataService: SensorDataService,
+    private db: AngularFireDatabase  // Make sure this line is correctly added
+  ) {}
 
   ngOnInit(): void {
     this.sensorDataService.getSensorData().subscribe(data => {
@@ -20,4 +28,69 @@ export class AppComponent implements OnInit {
       console.error("Erreur lors de la récupération des données : ", error);
     }); // Ferme le subscribe correctement
   } // Ferme le ngOnInit correctement
+
+  toggleLamps(): void {
+    const currentTime = new Date().toISOString(); // ISO string for the timestamp
+    const updates = {
+      '/lampes_allumees': this.lampStatus,
+      '/allumage_lampes_time': currentTime // Update the timestamp
+    };
+  
+    this.db.object('/').update(updates) // Updating at the root of the database path
+      .then(() => {
+        console.log(`Lampes ${this.lampStatus ? 'allumées' : 'éteintes'} à ${currentTime}!`);
+      })
+      .catch(err => {
+        console.error('Erreur lors de l\'allumage/éteignage des lampes:', err);
+      });
+  }
+
+  toggleVentilo(): void {
+    const currentTime = new Date().toISOString(); // ISO string for the timestamp
+    const updates = {
+      '/ventilateur_allume': this.ventiloStatus,
+      '/ventilateur_allume_time': currentTime // Update the timestamp
+    };
+  
+    this.db.object('/').update(updates) // Updating at the root of the database path
+      .then(() => {
+        console.log(`Ventilos ${this.ventiloStatus ? 'actionnés' : 'éteintes'} à ${currentTime}!`);
+      })
+      .catch(err => {
+        console.error('Erreur lors de l\'actionnement/éteignage des ventilos:', err);
+      });
+  }
+
+  toggleIrrigation(): void {
+    const currentTime = new Date().toISOString(); // ISO string for the timestamp
+    const updates = {
+      '/irrigation_active': this.irrigationStatus,
+      '/irrigation_active_time': currentTime // Update the timestamp
+    };
+  
+    this.db.object('/').update(updates) // Updating at the root of the database path
+      .then(() => {
+        console.log(`Irrigation ${this.irrigationStatus ? 'actionnés' : 'éteintes'} à ${currentTime}!`);
+      })
+      .catch(err => {
+        console.error('Erreur lors de l\'irrigation/éteignage:', err);
+      });
+  }
+
+  togglePorte(): void {
+    const currentTime = new Date().toISOString(); // ISO string for the timestamp
+    const updates = {
+      '/porte_ouverte': this.porteStatus,
+      '/porte_ouverte_time': currentTime // Update the timestamp
+    };
+  
+    this.db.object('/').update(updates) // Updating at the root of the database path
+      .then(() => {
+        console.log(`Porte ${this.porteStatus ? 'ouverte' : 'fermés'} à ${currentTime}!`);
+      })
+      .catch(err => {
+        console.error('Erreur lors de l\'ouverture/fermeture:', err);
+      });
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database'; 
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule, 
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig), 
     AngularFireDatabaseModule, 
   ],
